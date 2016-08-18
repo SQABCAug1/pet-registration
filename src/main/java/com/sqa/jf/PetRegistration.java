1d06e8c612a3b5c2d20376eda6d10a2049bcffc2
Jean-francois Nepton <jeanmfrancois@hotmail.com>
2016-08-17 20:21:57 -0700

    Create Pet Registration Class

---
+++ src/main/java/com/sqa/jf/PetRegistration.java
@@ -0,0 +1,13 @@
+package com.sqa.jf;
+
+public class PetRegistration {
+
+	public static void main(String[] args) {
+		// TODO Welcome the user
+		// TODO Ask for number of pets to register
+		// TODO Initial arrays with defined size
+		// TODO Iterate through the defined amount and ask for the pet details while populating the arrays
+		// TODO Display all animal details by iterating after the details were stored
+		// TODO Farewell the user
+	}
+}
